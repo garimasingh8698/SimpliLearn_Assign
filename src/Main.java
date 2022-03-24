29bc4a42256e67f315f9a2d494420ec0337166b7
Garima Singh <garimasingh8698@gmail.com>
2022-03-24 21:34:44 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+
+	/*Enter your desired Directory path */
+	public static final String path = "/home/garimasingh8698/Desktop/project/newdata/"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+
+}
